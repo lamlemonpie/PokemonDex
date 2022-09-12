b6fe01def6c390531f2ed28e0aa6c35bbfa55ef3
Alejandro Larraondo <alejandrolarraondo@gmail.com>
2022-09-12 15:06:23 -0500

    Initial Commit

---
+++ PokemonDex/PokemonDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonDexApp.swift
+//  PokemonDex
+//
+//  Created by Alejandro Larraondo on 9/12/22.
+//
+
+import SwiftUI
+
+@main
+struct PokemonDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
